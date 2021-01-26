17a7b71834f8f61901ccc020967722149b80d6c6
@slovacus <slovacus@gmail.com>
2021-01-26 14:53:34 -0500

    chore: implement code for internal zsh (#3)

---
+++ internal/main.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ksh
+# -*- coding: utf-8 -*-
+
+function goenv::internal::main::factory {
+    # shellcheck source=/dev/null
+    source "${ZSH_GOENV_PATH}"/internal/base.zsh
+    case "${OSTYPE}" in
+    darwin*)
+        # shellcheck source=/dev/null
+        source "${ZSH_GOENV_PATH}"/internal/osx.zsh
+        ;;
+    linux*)
+        # shellcheck source=/dev/null
+        source "${ZSH_GOENV_PATH}"/internal/linux.zsh
+      ;;
+    esac
+}
+
+goenv::internal::main::factory
+
+if ! type -p goenv > /dev/null; then goenv::internal::goenv::install; fi
